3f0aff8edfb919a1500f570f99d50490d755a6d1
Daniel Coronado <lautarodaniel.ldc@gmail.com>
2025-06-18 18:29:46 -0300

    completando el nav

--- src/componentes/formingresar.jsx
+++ src/componentes/formingresar.jsx
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"
 import "../css/formingresar.css"
 const Formingresar = ({titulo}) =>{ 
--- src/componentes/nav.jsx
+++ src/componentes/nav.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <img src={logo} alt="Logo EPET N°20" />
-      <a href="/">Inicio</a>
+      <a href="/envioCertificado">Inicio</a>
 
       <div className="dropdown">
         <a href="#">Alumnos ▼</a>
@@ -19,6 +19,16 @@ function Navbar() {
         <div className="dropdown-content">
           <a href="/docentes/opcion1">Opción 1</a>
           <a href="/docentes/opcion2">Opción 2</a>
+        </div>
+      </div>
+      <div className="dropdown">
+        <a href="/registro">registro</a>
+        <div className="dropdown-content">
+      </div>
+      </div>
+      <div className="dropdown">
+        <a href="/ingresar">inicio de sesion</a>
+        <div className="dropdown-content">
         </div>
       </div>
     </div>
