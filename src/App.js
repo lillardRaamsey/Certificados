de000a66f72c6da3ba50b5fecceeccd507d01695
abbylopez08 <abylop2020@gmail.com>
2025-08-31 02:02:34 -0300

    Se agregó fondo de inicio

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import CertificadoForm from './componentes/FormularioCertificados';
 import './App.css';
 import FormRegistro from './componentes/formRegistro';
 import Formingresar from './componentes/formingresar';
+import Inicio from './componentes/inicio';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
       <Navbar/>
         <Routes>
+            <Route path='/' element={<Inicio/>}/>
             <Route path="/envioCertificado" element={<CertificadoForm />}/>
             <Route path="/registro" element={<FormRegistro titulo="Registro de usuario" />}/>
             <Route path="/ingresar" element={<Formingresar />}/>
--- src/componentes/inicio.jsx
+++ src/componentes/inicio.jsx
@@ -1,4 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"
-import "../css/formRegistro.css"
+import image from '../img/iniciofondo.jpeg';
+import "../css/inicio.css"
 
+export default function Inicio() {
+  return (
+    <div className="fondo">
+        <img src={image} alt="Epet20" width={1271.9} height={577}/>
+    </div>
+  );
+}
