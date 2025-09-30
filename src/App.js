a70e182762a5c90a44ac1a7011dc927b456a8c00
abbylopez08 <abylop2020@gmail.com>
2025-09-30 13:08:48 -0300

    Modificación de inicio

--- src/App.js
+++ src/App.js
@@ -4,17 +4,30 @@ import './App.css';
 import FormRegistro from './componentes/formRegistro';
 import Formingresar from './componentes/formingresar';
 import Inicio from './componentes/inicio';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+
+function Layout() {
+  const location = useLocation();
+
+  const mostrarNavbar = location.pathname !== "/";
+
+  return (
+    <div>
+      {mostrarNavbar && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Inicio />} />
+        <Route path="/envioCertificado" element={<CertificadoForm />} />
+        <Route path="/registro" element={<FormRegistro titulo="Registro de usuario" />} />
+        <Route path="/ingresar" element={<Formingresar />} />
+      </Routes>
+    </div>
+  );
+}
+
 function App() {
   return (
     <Router>
-      <Navbar/>
-        <Routes>
-            <Route path='/' element={<Inicio/>}/>
-            <Route path="/envioCertificado" element={<CertificadoForm />}/>
-            <Route path="/registro" element={<FormRegistro titulo="Registro de usuario" />}/>
-            <Route path="/ingresar" element={<Formingresar />}/>
-        </Routes>
+      <Layout />
     </Router>
   );
 }
